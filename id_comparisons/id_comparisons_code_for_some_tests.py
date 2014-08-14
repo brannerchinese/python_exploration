8f3cd72bf35539086e63eb4fa5cac1876b0538d6
David Branner <brannerchinese@gmail.com>
2014-08-13 23:56:06 -0400

    add draft slides and code

---
+++ id_comparisons/id_comparisons_code_for_some_tests.py
@@ -0,0 +1,122 @@
+import array
+def test(choice):
+    choices = {'list': [1, 2, 3],
+            'tuple': (1, 2, 3),
+            'string': '123',
+            'array': array.array('i', [1, 2, 3]),
+            'bytes': bytes(123)
+            }
+    x = choices[choice]
+    print id(x[:]) == id(x[:])
+    y1 = x[:]
+    y2 = x[:]
+    print id(y1) == id(y2)
+    print id(y1) == id(y1)
+    print id(x[:])
+    print id(x[:])
+    print id(x[:])
+    print id(x[:])
+
+test('list')
+test('array')
+test('tuple')
+test('string')
+test('bytes')
+
+################
+
+def test_list():
+    print id([1, 2, 3][:]) == id([1, 2, 3][:])
+    y1 = [1, 2, 3][:]
+    y2 = [1, 2, 3][:]
+    print id(y1) == id(y2)
+    print id(y1) == id(y1)
+    print id([1, 2, 3][:])
+    print id([1, 2, 3][:])
+    print id([1, 2, 3][:])
+    print id([1, 2, 3][:])
+
+def test_tuple():
+    print id((1, 2, 3)[:]) == id((1, 2, 3)[:])
+    y1 = (1, 2, 3)[:]
+    y2 = (1, 2, 3)[:]
+    print id(y1) == id(y2)
+    print id(y1) == id(y1)
+    print id((1, 2, 3)[:])
+    print id((1, 2, 3)[:])
+    print id((1, 2, 3)[:])
+    print id((1, 2, 3)[:])
+
+def test_array():
+    print id(array.array('i', [1, 2, 3])[:]) == id(array.array('i', [1, 2, 3])[:])
+    y1 = array.array('i', [1, 2, 3])[:]
+    y2 = array.array('i', [1, 2, 3])[:]
+    print id(y1) == id(y2)
+    print id(y1) == id(y1)
+    print id(array.array('i', [1, 2, 3])[:])
+    print id(array.array('i', [1, 2, 3])[:])
+    print id(array.array('i', [1, 2, 3])[:])
+    print id(array.array('i', [1, 2, 3])[:])
+
+def test_string():
+    print id('123'[:]) == id('123'[:])
+    y1 = '123'[:]
+    y2 = '123'[:]
+    print id(y1) == id(y2)
+    print id(y1) == id(y1)
+    print id('123'[:])
+    print id('123'[:])
+    print id('123'[:])
+    print id('123'[:])
+
+def test_bytes():
+    print id(bytes(123)[:]) == id(bytes(123)[:])
+    y1 = bytes(123)[:]
+    y2 = bytes(123)[:]
+    print id(y1) == id(y2)
+    print id(y1) == id(y1)
+    print id(bytes(123)[:])
+    print id(bytes(123)[:])
+    print id(bytes(123)[:])
+    print id(bytes(123)[:])
+
+test_list()
+test_tuple()
+test_array()
+test_string()
+test_bytes()
+
+################
+
+def test_list2():
+    print id([1, 2, 3][:]) == id([1, 2, 3][:])
+    y1 = [1, 2, 3][:]
+    y2 = [1, 2, 3][:]
+    print id(y1) == id(y2)
+    print id(y1) == id(y1)
+    print id([1, 2, 3][:])
+    print id([1, 2, 3][:])
+    print id([1, 2, 3][:])
+    print id([1, 2, 3][:])
+    print id([1, 2, 3][:])
+    print id([1, 2, 3][:])
+    print id([1, 2, 3][:])
+    print id([1, 2, 3][:])
+
+def test_array2():
+    print id(array.array('i', [1, 2, 3])[:]) == id(array.array('i', [1, 2, 3])[:])
+    y1 = array.array('i', [1, 2, 3])[:]
+    y2 = array.array('i', [1, 2, 3])[:]
+    print id(y1) == id(y2)
+    print id(y1) == id(y1)
+    print id(array.array('i', [1, 2, 3])[:])
+    print id(array.array('i', [1, 2, 3])[:])
+    print id(array.array('i', [1, 2, 3])[:])
+    print id(array.array('i', [1, 2, 3])[:])
+    print id(array.array('i', [1, 2, 3])[:])
+    print id(array.array('i', [1, 2, 3])[:])
+    print id(array.array('i', [1, 2, 3])[:])
+    print id(array.array('i', [1, 2, 3])[:])
+
+test_list2()
+test_array2()
